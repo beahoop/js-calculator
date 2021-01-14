8de154520b5933f6f15d20dfc55a6e68faee3f4c
 Mac <Mac@MacBook-Pro.local>
2021-01-14 14:22:39 -0500

    can change cal screen

--- scripts/main.js
+++ scripts/main.js
@@ -10,44 +10,51 @@
 
 //numbers
 const ozero = document.querySelector(`[value='0']`);
-  console.log(ozero);
+  // console.log(ozero);
 const one = document.querySelector(`[value='1']`);
-  console.log(one);
+  // console.log(one);
 const two = document.querySelector(`[value='2']`);
-  console.log(two);
+  // console.log(two);
 const three = document.querySelector(`[value='3']`);
-  console.log(three);
+  // console.log(three);
 const four = document.querySelector(`[value='4']`);
-  console.log(four);
+  // console.log(four);
 const five = document.querySelector(`[value='5']`);
-  console.log(five);
+  // console.log(five);
 const six = document.querySelector(`[value='6']`);
-  console.log(six);
+  // console.log(six);
 const seven = document.querySelector(`[value='7']`);
-  console.log(seven);
+  // console.log(seven);
 const eight = document.querySelector(`[value='8']`);
-  console.log(eight);
+  // console.log(eight);
 const nine = document.querySelector(`[value='9']`);
-  console.log(nine);
+  // console.log(nine);
 
 //operator
 const percent = document.querySelector(`[value='percent']`);
-  console.log(percent);
+  // console.log(percent);
 const divide = document.querySelector(`[value='/']`);
-  console.log(divide);
+  // console.log(divide);
 const times = document.querySelector(`[value='*']`);
-  console.log(times);
+  // console.log(times);
   const minus = document.querySelector(`[value='-']`);
-    console.log(minus);
+    // console.log(minus);
   const plus = document.querySelector(`[value='+']`);
-    console.log(plus);
+    // console.log(plus);
   const decimal = document.querySelector(`[value='.']`);
-    console.log(decimal);
+    // console.log(decimal);
   const equal = document.querySelector(`[value='=']`);
-    console.log(equal);
+    // console.log(equal);
 
 //function
 const clear = document.querySelector('.clear') ;
-  console.log(clear);
+  // console.log(clear);
 const plusMinus = document.querySelector('.plus-minus');
-  console.log(plusMinus);
+  // console.log(plusMinus);
+
+// <input type="text" class="calculator-screen" value="0" disabled />
+// const calscreen = document.querySelector(`.calculator-screen`)
+// calscreen.
+
+  console.log(document.querySelector('.calculator-screen').value);
+document.querySelector('.calculator-screen').value = eight.innerHTML

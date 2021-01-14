f2b1d82b91124da5c2203f2d18d4b460fdd610ae
 Mac <Mac@MacBook-Pro.local>
2021-01-14 13:45:36 -0500

    add variables

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+//write flow chart
+//atomic level of whatever you are doing
+// what do you know how to do
+// do all the little things
+
+
+//set variable to the buttons like query selector to the [CHECK]
+// just display one number , use an eventlister
+//try to get 2+2
+
+//numbers
+const ozero = document.querySelector(`[value='0']`);
+  console.log(ozero);
+const one = document.querySelector(`[value='1']`);
+  console.log(one);
+const two = document.querySelector(`[value='2']`);
+  console.log(two);
+const three = document.querySelector(`[value='3']`);
+  console.log(three);
+const four = document.querySelector(`[value='4']`);
+  console.log(four);
+const five = document.querySelector(`[value='5']`);
+  console.log(five);
+const six = document.querySelector(`[value='6']`);
+  console.log(six);
+const seven = document.querySelector(`[value='7']`);
+  console.log(seven);
+const eight = document.querySelector(`[value='8']`);
+  console.log(eight);
+const nine = document.querySelector(`[value='9']`);
+  console.log(nine);
+
+//operator
+const percent = document.querySelector(`[value='percent']`);
+  console.log(percent);
+const divide = document.querySelector(`[value='/']`);
+  console.log(divide);
+const times = document.querySelector(`[value='*']`);
+  console.log(times);
+  const minus = document.querySelector(`[value='-']`);
+    console.log(minus);
+  const plus = document.querySelector(`[value='+']`);
+    console.log(plus);
+  const decimal = document.querySelector(`[value='.']`);
+    console.log(decimal);
+  const equal = document.querySelector(`[value='=']`);
+    console.log(equal);
+
+//function
+const clear = document.querySelector('.clear') ;
+  console.log(clear);
+const plusMinus = document.querySelector('.plus-minus');
+  console.log(plusMinus);
